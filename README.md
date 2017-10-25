73f14a939f398024086df33c022f87aef5dd76ac
Te Vallee <astronomemusic@gmail.com>
2017-10-25 13:27:32 -1000

    documented usage/dev/build process

---
+++ README.md
@@ -0,0 +1,52 @@
+# Nav-Login-Status
+
+## Usage
+
+include `navLoginStatus.js` and `navLoginStatus.css` in your airship project:
+
+```
+<head>
+	<link rel="stylesheet" type="text/css" href="/compartments/assets/styles/navLoginStatus.css">
+</head>
+<body>
+	<script src="/compartments/assets/scripts/navLoginStatus.js"></script>
+</body>
+```
+
+add the element container:
+
+```
+<div id="nav-login-status"></div>
+```
+
+note that the element's `id` but be `nav-login-status`.
+
+create an instance of NavLoginStatus with the airshipcms domain:
+
+```
+new NavLoginStatus('domain.airshipcms.io');
+```
+
+## Development
+
+install dependencies
+
+```
+yarn install
+```
+
+start dev server
+
+```
+gulp
+```
+
+this serves `src/` on `localhost:8080`
+
+## Build
+
+```
+gulp build
+```
+
+necessary files will build into `dist/`
--- src/app.js
+++ src/app.js
@@ -1,36 +1,35 @@
 class NavLoginStatus {
-
   constructor(domain) {
-    this.id_token = localStorage.getItem('id_token');
-    this.element = document.getElementById('nav-login-status');
+    this.id_token = localStorage.getItem("id_token");
+    this.element = document.getElementById("nav-login-status");
     this.domain = domain;
 
     this.checkContainer();
   }
 
   render() {
-    this.signInButtonEl = document.createElement('a')
-    this.element.appendChild(this.signInButtonEl)
-    this.signInButtonEl.innerHTML = 'Sign In';
-    this.signInButtonEl.href = '/login';
-    this.signInButtonEl.className = 'nav-login-status-signin'
+    this.signInButtonEl = document.createElement("a");
+    this.element.appendChild(this.signInButtonEl);
+    this.signInButtonEl.innerHTML = "Sign In";
+    this.signInButtonEl.href = "/login";
+    this.signInButtonEl.className = "nav-login-status-signin";
 
-    this.dropDownEl = document.createElement('ul');    
-    this.userEmailEl = document.createElement('li');
-    let logoutEl = document.createElement('li');
+    this.dropDownEl = document.createElement("ul");
+    this.userEmailEl = document.createElement("li");
+    let logoutEl = document.createElement("li");
 
     this.element.appendChild(this.dropDownEl);
     this.dropDownEl.appendChild(this.userEmailEl);
     this.dropDownEl.appendChild(logoutEl);
-    this.dropDownEl.className = 'nav-login-status-dropdown hidden';
+    this.dropDownEl.className = "nav-login-status-dropdown hidden";
 
-    logoutEl.innerHTML = 'Logout';
-    logoutEl.className = 'nav-login-status-logout';
-    logoutEl.addEventListener('click', () => this.logout());
+    logoutEl.innerHTML = "Logout";
+    logoutEl.className = "nav-login-status-logout";
+    logoutEl.addEventListener("click", () => this.logout());
 
-    if(this.id_token !== null) {
+    if (this.id_token !== null) {
       this.getProfile((err, xhr) => {
-        if(xhr.status === 200){
+        if (xhr.status === 200) {
           this.userEmailEl.innerHTML = JSON.parse(xhr.response).email;
           this.toggleElements();
         }
@@ -39,40 +38,40 @@ class NavLoginStatus {
   }
 
   toggleElements() {
-    this.signInButtonEl.classList.toggle('hidden');
-    this.dropDownEl.classList.toggle('hidden');
+    this.signInButtonEl.classList.toggle("hidden");
+    this.dropDownEl.classList.toggle("hidden");
   }
 
-  getProfile(done){
+  getProfile(done) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://${this.domain}/api/users/profile`);
-    xhr.setRequestHeader('Authorization', `bearer ${this.id_token}`);
+    xhr.open("GET", `https://${this.domain}/api/users/profile`);
+    xhr.setRequestHeader("Authorization", `bearer ${this.id_token}`);
     xhr.onload = () => done(null, xhr);
     xhr.onerror = () => done(xhr.xhr);
     xhr.send();
   }
 
-  logout(){
+  logout() {
     localStorage.clear();
     this.toggleElements();
   }
 
   isValidDomain(domain) {
     let valid = false;
-    let substring = '.airshipcms.io';
-    let substring2 = '.airshipcms-alpha.io';
-    if(domain.includes(substring) || domain.includes(substring2)){
+    let substring = ".airshipcms.io";
+    let substring2 = ".airshipcms-alpha.io";
+    if (domain.includes(substring) || domain.includes(substring2)) {
       valid = true;
     }
     return valid;
   }
 
   checkContainer() {
-    if(this.domain === undefined || !this.isValidDomain(this.domain)){
-      throw new Error('valid airship domain required');
+    if (this.domain === undefined || !this.isValidDomain(this.domain)) {
+      throw new Error("valid airship domain required");
     }
-    if(this.element === null){
-      throw new Error('Nav Login Status container not found');
+    if (this.element === null) {
+      throw new Error("Nav Login Status container not found");
     } else {
       this.render();
     }
