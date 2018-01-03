37acc87dd0abfab04c3f2f02695f5e323ee370d4
Te Vallee <astronomemusic@gmail.com>
2018-01-02 15:34:56 -1000

    new build

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = [
       ]
     },
     plugins: [
-      // new MinifyPlugin()
+      new MinifyPlugin()
     ],
     devServer: {
       contentBase: path.join(__dirname, 'dist'),
